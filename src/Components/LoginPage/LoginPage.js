592243b7e41c86c161144eddf7a4da31cc8f4714
Andy Du Ly <andy.duly@benchedms.ca>
2021-03-25 17:19:35 -0400

    weird prod issue

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -21,10 +21,16 @@ const LoginPage = (props) => {
 
     try {
       const res = await LoginHook(email, password);
+      
+      console.log(res);
 
-      localStorage.setItem("token", res.data.token);
-      localStorage.setItem("userID", res.data.user._id);
-      props.history.push("/");
+      if (res.status === 200) {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("userID", res.data.user._id);
+        props.history.push("/");
+      } else {
+        alert("Auth Failed. Check credentials");
+      }
     } catch (err) {
       console.log(err);
     }
